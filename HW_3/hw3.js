5ba3592716e14412a76779127e7f7a9ab3bcfae3
root <root@HELL-CAT.localdomain>
2019-06-08 12:33:35 +0300

    1

--- HW_3/hw3.js
+++ HW_3/hw3.js
@@ -1,18 +1,65 @@
 
-let area = document.querySelector('#area');
-let colorInput = document.querySelector('#colorInp');
+let area1 = document.querySelector('#area1');
+let colorInput1 = document.querySelector('#colorInp1');
+let area2 = document.querySelector('#area2');
+let form1=document.form1.figure;
+let form_lengths=document.form_lengths.length;
+var isRadiusExists = true;
+console.log(form_lengths);
 
-
-let clickHandler = function(event) {
-  if (event.target.id !== 'area') return;
+let clickHandler = function(event,) {
   let newFigure = document.createElement('div');
-  let figureColor = colorInput.value;
-  // newFigure.className = 'figure';
-  newFigure.classList.add('figure');
+  let figureColor = colorInput1.value;
+  newFigure.classList.add('figure1');
   newFigure.style.top = event.offsetY + 'px';
   newFigure.style.left = event.offsetX + 'px';
   newFigure.style.backgroundColor = figureColor;
-  area.appendChild(newFigure);
+  if (document.getElementById("width").value !== null) {
+    newFigure.style.width=document.getElementById("width").value + 'px';
+  }
+  if (document.getElementById("height").value !== null) {
+    newFigure.style.height=document.getElementById("height").value + 'px';
+  }
+  if (isRadiusExists) {
+    newFigure.style.radius='50%';
+  }
+  document.querySelector("#"+this.id).appendChild(newFigure);
+  console.log(newFigure);
+  
 }
 
-area.addEventListener('click', clickHandler);
+area1.addEventListener('click', clickHandler);
+area2.addEventListener('click', clickHandler);
+
+for(let i=0; i<form1.length; i++ ){
+  form1[i].addEventListener('change', function(event) {
+
+    // console.log(this.value) ;
+    switch (this.value) {
+      case 'Rectangle':
+          document.getElementById("width").style.display="inline";
+          document.getElementById("height").style.display="inline";
+          isRadiusExists=false;
+        break;
+      case 'Square':
+          document.getElementById("width").style.display="inline";
+          document.getElementById("height").style.display="none";
+          document.getElementById("height").value=null;
+          isRadiusExists=false;
+        break;
+      case 'Circle':
+          document.getElementById("width").style.display="inline";
+          document.getElementById("height").style.display="none";
+          document.getElementById("height").value=null;
+          isRadiusExists=true;
+        break;
+      case 'Oval':
+          document.getElementById("width").style.display="inline";
+          document.getElementById("height").style.display="inline";
+          isRadiusExists=true;
+        break;
+                
+    }
+    }, false);
+
+}
